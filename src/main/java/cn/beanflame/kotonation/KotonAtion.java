cc747c443d80b083e5bfde4820f40566e51f70d0
beanflame <beanflame@qq.com>
2022-01-21 23:13:49 +0800

    Update and rename src/main/java/com/example/examplemod/ExampleMod.java to src/main/java/cn/beanflame/kotonation/KotonAtion.java

--- src/main/java/cn/beanflame/kotonation/KotonAtion.java
+++ src/main/java/cn/beanflame/kotonation/KotonAtion.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package cn.beanflame.kotonation;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,9 +19,9 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
+@Mod(KotonAtion.ID)
+public class KotonAtion {
+    public static final String ID = "koton_ation";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
